63c1bf675497dbae605efd849606ec206e7fbbf7
kozhemyakin <pctdoc@gmail.com>
2023-01-13 01:03:43 +0300

    feat: add krestik to burger menu

--- plants/scripts/index.js
+++ plants/scripts/index.js
@@ -1,9 +1,10 @@
 console.log('Вёрстка валидная +10,\nВёрстка семантическая +20,\nВёрстка соответствует макету +48,\nТребования к css + 12,\nИнтерактивность, реализуемая через css +20.\nИтоговая оценка: 100 баллов')
 
 function openMenu() {
-    document.querySelector('.opened-mobile-menu').classList.add('activated')
+    document.querySelector('.opened-mobile-menu').classList.toggle('activated')
 }
 
 function closeMenu() {
-    document.querySelector('.opened-mobile-menu').classList.remove('activated')
+    document.querySelector('.opened-mobile-menu').classList.toggle('activated')
   }
+  
